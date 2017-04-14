aeae09b52f26547bfac5fa52f6a2daf1df760a28
tsuzan007 <tsuzanprajapati@gmail.com>
2017-04-13 21:10:11 -0400

    1

---
+++ app/src/main/java/com/sujan/mykitaab/LogInActivity.java
@@ -0,0 +1,14 @@
+package com.sujan.mykitaab;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogInActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
