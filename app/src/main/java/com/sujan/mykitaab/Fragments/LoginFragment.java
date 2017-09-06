327a4aeb85d589bc155cfafea6e538198e722697
tsuzan007 <tsuzanprajapati@gmail.com>
2017-09-06 16:54:49 -0400

    If not signed out in previous session, user is taken to homepage directly. Reformatting codes

--- app/src/main/java/com/sujan/mykitaab/Fragments/LoginFragment.java
+++ app/src/main/java/com/sujan/mykitaab/Fragments/LoginFragment.java
@@ -1,10 +1,15 @@
 package com.sujan.mykitaab.Fragments;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.app.Fragment;
 import android.text.TextUtils;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +27,9 @@ import com.sujan.mykitaab.Presenter.MyKitabPresenter;
 import com.sujan.mykitaab.Presenter.PresenterContract;
 import com.sujan.mykitaab.R;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -35,21 +43,26 @@ public class LoginFragment extends Fragment {
     private PresenterContract.LoginInfoInputToPresenter loginInfoInputToPresenter;
     private MyKitabPresenter myKitabPresenter;
 
-    public void setLoginInfoInputToPresenter(PresenterContract.LoginInfoInputToPresenter loginInfoInputToPresenter){
-        this.loginInfoInputToPresenter=loginInfoInputToPresenter;
+
+    public void setLoginInfoInputToPresenter(PresenterContract.LoginInfoInputToPresenter loginInfoInputToPresenter) {
+        this.loginInfoInputToPresenter = loginInfoInputToPresenter;
     }
 
 
-    @BindView(R.id.edittext_username) EditText username;
-    @BindView(R.id.edittext_password) EditText password;
-    @BindView(R.id.button_signin) Button signin;
-    @BindView(R.id.button_facebooklogin) LoginButton facebooklogin;
+    @BindView(R.id.edittext_username)
+    EditText username;
+    @BindView(R.id.edittext_password)
+    EditText password;
+    @BindView(R.id.button_signin)
+    Button signin;
+    @BindView(R.id.button_facebooklogin)
+    LoginButton facebooklogin;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        myKitabPresenter=new MyKitabPresenter(loginInfoInputToPresenter);
+        myKitabPresenter = new MyKitabPresenter(loginInfoInputToPresenter);
 
 
     }
@@ -57,15 +70,20 @@ public class LoginFragment extends Fragment {
 
     @Override
     public void onResume() {
+
         super.onResume();
+        if (facebooklogin.getText().toString().equals("Log out")) {
+            Intent intent = new Intent(getActivity(), MainActivity.class);
+            startActivity(intent);
+        }
     }
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.login,container,false);
-        ButterKnife.bind(this,view);
+        View view = inflater.inflate(R.layout.login, container, false);
+        ButterKnife.bind(this, view);
 //        try {
 //            PackageInfo info = getActivity().getPackageManager().getPackageInfo(
 //                    "com.sujan.mykitaab",
@@ -80,23 +98,22 @@ public class LoginFragment extends Fragment {
 //        } catch (NoSuchAlgorithmException e) {
 //
 //        }
-        facebooklogin.setReadPermissions( "public_profile", "user_birthday","email","user_posts","user_friends","read_custom_friendlists");
+        facebooklogin.setReadPermissions("public_profile", "user_birthday", "email", "user_posts", "user_friends", "read_custom_friendlists");
         facebooklogin.setFragment(this);
         callbackManager = CallbackManager.Factory.create();
         return view;
     }
 
 
-
-    @OnClick({R.id.button_facebooklogin,R.id.button_signin})
-    public void onClickbutton(View view){
-        if(view.getId()==R.id.button_facebooklogin){
+    @OnClick({R.id.button_facebooklogin, R.id.button_signin})
+    public void onClickbutton(View view) {
+        if (view.getId() == R.id.button_facebooklogin) {
             callbackManager = CallbackManager.Factory.create();
             facebooklogin.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
                 @Override
                 public void onSuccess(LoginResult loginResult) {
                     myKitabPresenter.onSuccess(loginResult);
-                    Intent intent=new Intent(getActivity(),MainActivity.class);
+                    Intent intent = new Intent(getActivity(), MainActivity.class);
                     startActivity(intent);
 
 
@@ -116,27 +133,24 @@ public class LoginFragment extends Fragment {
             });
 
 
-        }
-        else if(view.getId()==R.id.button_signin){
-            String name=username.getText().toString();
-            if(!TextUtils.isEmpty(name)){
-                Intent intent=new Intent(getActivity(),MainActivity.class);
-                intent.putExtra("username",name);
+        } else if (view.getId() == R.id.button_signin) {
+            String name = username.getText().toString();
+            if (!TextUtils.isEmpty(name)) {
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                intent.putExtra("username", name);
                 startActivity(intent);
 
+            } else {
+                DialogFragment dialogFragment = new DialogFragment();
+                dialogFragment.show(getFragmentManager(), "message dialog");
             }
-            else{
-                DialogFragment dialogFragment=new DialogFragment();
-                dialogFragment.show(getFragmentManager(),"message dialog");
-            }
-
 
 
         }
     }
 
     @OnClick(R.id.textview_signup)
-    public void onSignupClicked(TextView textView){
+    public void onSignupClicked(TextView textView) {
         /*
         get userid, name, email address from user and pass it to the function
          */
