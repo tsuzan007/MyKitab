c848e8519e3ab8599ac49121471b650e0518066e
tsuzan007 <tsuzanprajapati@gmail.com>
2017-09-08 20:15:43 -0400

    Shared Preferences added to the class to store user information. The information are then reloaded when view is active.

--- app/src/main/java/com/sujan/mykitaab/MainActivity.java
+++ app/src/main/java/com/sujan/mykitaab/MainActivity.java
@@ -1,6 +1,10 @@
 package com.sujan.mykitaab;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.PersistableBundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,10 +15,8 @@ import android.text.TextUtils;
 import android.widget.Button;
 
 import com.facebook.login.LoginManager;
-import com.sujan.mykitaab.HelperClass.MessageEvent;
-import com.sujan.mykitaab.HelperClass.User_WithFacebook;
-import com.sujan.mykitaab.Presenter.MyKitabPresenter;
-import com.sujan.mykitaab.Presenter.PresenterContract;
+import com.sujan.mykitaab.POJOClasses.MessageEvent;
+import com.sujan.mykitaab.POJOClasses.User_WithFacebook;
 
 
 import org.greenrobot.eventbus.EventBus;
@@ -24,7 +26,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity implements PresenterContract.PublishView {
+public class MainActivity extends AppCompatActivity {
     @BindView(R.id.app_toolbar)
     Toolbar toolbar;
     @BindView(R.id.drawer)
@@ -36,8 +38,13 @@ public class MainActivity extends AppCompatActivity implements PresenterContract
     @BindView(R.id.logout)
     Button logout;
     ActionBarDrawerToggle actionBarDrawerToggle;
-    MyKitabPresenter myKitabPresenter;
-    MessageEvent messageEvent;
+
+    private User_WithFacebook user_withFacebook;
+
+    Intent intent;
+
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
 
 
     @Override
@@ -45,7 +52,6 @@ public class MainActivity extends AppCompatActivity implements PresenterContract
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        myKitabPresenter = new MyKitabPresenter(this);
         actionBarDrawerToggle = new ActionBarDrawerToggle(
                 this,
                 drawerLayout, toolbar,
@@ -53,9 +59,9 @@ public class MainActivity extends AppCompatActivity implements PresenterContract
                 R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
 
-
         //calling sync state is necessay or else your hamburger icon wont show up
-        actionBarDrawerToggle.syncState();
+        actionBarDrawerToggle.syncState();EventBus.getDefault().unregister(this);
+
 
     }
 
@@ -69,14 +75,16 @@ public class MainActivity extends AppCompatActivity implements PresenterContract
     @OnClick(R.id.logout)
     public void onlogoutButtonClicked(Button button) {
         LoginManager.getInstance().logOut();
-        finish();
+        Intent intent = new Intent(this, LogInActivity.class);
+        startActivity(intent);
+
 
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Intent intent = getIntent();
+        intent = getIntent();
         String name = "";
         name = intent.getStringExtra("username");
         if (TextUtils.isEmpty(name)) {
@@ -85,7 +93,26 @@ public class MainActivity extends AppCompatActivity implements PresenterContract
 
         }
         navigationView.getMenu().getItem(0).setTitle(name);
+        sharedPreferences=getSharedPreferences("userinfo", Context.MODE_PRIVATE);
+        editor=sharedPreferences.edit();
         EventBus.getDefault().register(this);
+
+    }
+
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        outState.putParcelable("user", user_withFacebook);
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        navigationView.getMenu().getItem(0).setTitle(((User_WithFacebook) savedInstanceState.getParcelable("user")).getName());
+        navigationView.getMenu().getItem(1).setTitle(((User_WithFacebook) savedInstanceState.getParcelable("user")).getEmail_id());
+        navigationView.getMenu().getItem(2).setTitle(((User_WithFacebook) savedInstanceState.getParcelable("user")).getDate_of_birth());
+
 
     }
 
@@ -94,22 +121,28 @@ public class MainActivity extends AppCompatActivity implements PresenterContract
         super.onStop();
         EventBus.getDefault().unregister(this);
 
+
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-    }
-
-    @Override
-    public void publish_userInfo(User_WithFacebook user) {
+            navigationView.getMenu().getItem(0).setTitle(sharedPreferences.getString("name","Name"));
+            navigationView.getMenu().getItem(1).setTitle(sharedPreferences.getString("emailid","email"));
+            navigationView.getMenu().getItem(2).setTitle(sharedPreferences.getString("dob","xx-xx-xxxx"));
 
 
     }
+
 
     @Subscribe
     public void updatenavigation(User_WithFacebook user_withFacebook) {
+        this.user_withFacebook = user_withFacebook;
+        editor.putString("name",user_withFacebook.getName());
+        editor.putString("emailid",user_withFacebook.getEmail_id());
+        editor.putString("dob",user_withFacebook.getDate_of_birth());
+        editor.commit();
         navigationView.getMenu().getItem(0).setTitle(user_withFacebook.getName());
         navigationView.getMenu().getItem(1).setTitle(user_withFacebook.getEmail_id());
         navigationView.getMenu().getItem(2).setTitle(user_withFacebook.getDate_of_birth());
