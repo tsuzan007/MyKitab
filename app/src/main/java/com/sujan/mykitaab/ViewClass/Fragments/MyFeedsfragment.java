666389ccc4bad590f8feef7352d38fb338e38be9
tsuzan007 <tsuzanprajapati@gmail.com>
2017-09-13 22:35:40 -0400

    Added SharedPreference to save user's status: Logged in or logged out.

--- app/src/main/java/com/sujan/mykitaab/ViewClass/Fragments/MyFeedsfragment.java
+++ app/src/main/java/com/sujan/mykitaab/ViewClass/Fragments/MyFeedsfragment.java
@@ -1,17 +1,25 @@
 package com.sujan.mykitaab.ViewClass.Fragments;
 
+import android.app.Fragment;
+import android.app.IntentService;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.sujan.mykitaab.POJOClasses.FeedClass;
 import com.sujan.mykitaab.POJOClasses.MessageEvent;
 import com.sujan.mykitaab.POJOClasses.User_WithFacebook;
@@ -26,18 +34,22 @@ import org.greenrobot.eventbus.Subscribe;
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.os.Looper.getMainLooper;
+
 /**
  * Created by macbookpro on 4/16/17.
  */
 
 public class MyFeedsfragment extends Fragment implements MVPContracts.PublishToView {
-    RecyclerView recyclerView;
-    MyRecyclerViewAdapter adapter;
-    StaggeredGridLayoutManager stagManager;
-    SwipeRefreshLayout swipeRefreshLayout;
-    List<FeedClass> mylist = new ArrayList<>();
-    LinearLayoutManager linearLayoutManager;
-    MyKitabPresenter myKitabPresenter;
+    private RecyclerView recyclerView;
+    private MyRecyclerViewAdapter adapter;
+    private StaggeredGridLayoutManager stagManager;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private List<FeedClass> mylist = new ArrayList<>();
+    private LinearLayoutManager linearLayoutManager;
+    private MyKitabPresenter myKitabPresenter;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     public void onAttach(Context context) {
@@ -54,18 +66,41 @@ public class MyFeedsfragment extends Fragment implements MVPContracts.PublishToV
         recyclerView = (RecyclerView) view.findViewById(R.id.myrecyclerview);
         swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeRefreshLayout);
         linearLayoutManager = new LinearLayoutManager(getActivity());
+        sharedPreferences=getActivity().getSharedPreferences("userstatus",Context.MODE_PRIVATE);
+        editor=sharedPreferences.edit();
+        editor.putBoolean("userStatus",true);
+        editor.commit();
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 // Refresh items
                 fetchTimelineAsync(0);
-
-
             }
         });
-
-
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        myKitabPresenter.start();
+        EventBus.getDefault().register(this);
+        adapter = new MyRecyclerViewAdapter(getActivity(), mylist);
+        stagManager = new StaggeredGridLayoutManager(2, 1);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(linearLayoutManager);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        myKitabPresenter.onStop();
+        EventBus.getDefault().unregister(this);
     }
 
 
@@ -84,44 +119,13 @@ public class MyFeedsfragment extends Fragment implements MVPContracts.PublishToV
         // Send the network request to fetch the updated data
         // `client` here is an instance of Android Async HTTP
         // getHomeTimeline is an example endpoint.
+
         mylist.clear();
         myKitabPresenter.onLoadFeeds();
         swipeRefreshLayout.setRefreshing(false);
         adapter.notifyDataSetChanged();
-
-
     }
 
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        myKitabPresenter.start();
-        EventBus.getDefault().register(this);
-        adapter = new MyRecyclerViewAdapter(getActivity(), mylist);
-        stagManager = new StaggeredGridLayoutManager(2, 1);
-
-
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(linearLayoutManager);
-
-
-
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        myKitabPresenter.onStop();
-        EventBus.getDefault().unregister(this);
-
-
-    }
 
     @Override
     public void publish_userInfo(User_WithFacebook user) {
